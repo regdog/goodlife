a58a24b0a048ce0ce7b7df09eac22afd5d6d628d
Yang Wang <regdog101@yahoo.com>
2011-09-05 00:24:54 +0800

    paperclip migration

--- db/migrate/20110904120910_create_uploads.rb
+++ db/migrate/20110904120910_create_uploads.rb
@@ -1,14 +1,11 @@
 class CreateUploads < ActiveRecord::Migration
   def self.up
     create_table :uploads do |t|
-      t.string  :filename
-      t.integer :width, :null => false, :default => 0
-      t.integer :height, :null => false, :default => 0
+      t.references :attachable, :polymorphic => true
+      t.string  :upload_file_name
+      t.string  :upload_content_type
+      t.integer :upload_size
       t.string  :type
-      t.integer :parent_id
-      t.string  :content_type
-      t.string  :thumbnail
-      t.integer :size
       t.timestamps
     end
   end
