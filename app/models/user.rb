ecf085f755f4ec2e06d9b64bbae74a0edb354412
lyslim <lyslim@gmail.com>
2011-09-14 23:51:06 +0800

    invitation

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :name, :password, :password_confirmation, :remember_me
   validates_presence_of :name, :if => :name_required?
+  validates_uniqueness_of :name
   
   has_many :authentications, :dependent => :destroy
   has_many :checkins
